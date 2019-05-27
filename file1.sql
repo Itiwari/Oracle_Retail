48b12c7f15ed63fd88fb9c71095c3610d57fe341
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-05-27 15:42:13 +0530

    Update file1.sql

--- file1.sql
+++ file1.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE mydb;
 CREATE TABLE mytable ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO mytable VALUES ( 1, 'Will' );
-INSERT INTO mytable VALUES ( 2, 'Marry' );
-INSERT INTO mytable VALUES ( 3, 'Dean' );
-SELECT id, name FROM mytable WHERE id = 1;
+INSERT INTO mytable VALUES ( 1, 'A' );
+INSERT INTO mytable VALUES ( 2, 'B' );
+INSERT INTO mytable VALUES ( 3, 'C' );
+SELECT id, name FROM mytable WHERE id > 1;
