298b70b8ae2c1a75fdb8645526210188263bb5ae
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-05-27 15:41:14 +0530

    Update file1.sql

--- file1.sql
+++ file1.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE mydb;
-
+USE mydb;
+CREATE TABLE mytable ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO mytable VALUES ( 1, 'Will' );
+INSERT INTO mytable VALUES ( 2, 'Marry' );
+INSERT INTO mytable VALUES ( 3, 'Dean' );
+SELECT id, name FROM mytable WHERE id = 1;
